ca96a6ae935610b6d7d54693b2a6e0952d9fe202
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-22 23:53:45 +0700

    menambah tampilan active status pada navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,28 @@
+@section('head')
+    <style>
+        .navbar .nav-link.active {
+            background-color: transparent !important;
+            font-weight: bold !important;
+            color: black !important;
+        }
+    </style> 
+@endsection
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Laravel7</a>
 
     <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">Home</a>
+        <li class="nav-item">
+            <a class="nav-link{{request()->is('/') ? ' active' : ''}}" href="/">Home</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="contact">Contact</a>
+            <a class="nav-link{{request()->is('contact') ? ' active' : ''}}" href="contact">Contact</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="about">About</a>
+            <a class="nav-link{{request()->is('about') ? ' active' : ''}}" href="about">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="login">Login</a>
+            <a class="nav-link{{request()->is('login') ? ' active' : ''}}" href="login">Login</a>
         </li>
     </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('contact', function (Request $request) {
-    // return $request->path(); digunakan untuk menampilkan path dari route
-    // return request()->path(); digunakan untuk menampilkan path dari route
-
-    // boolean untuk mengecek apakah path dari route sesuai dengan parameter
-    // return request()->path() == 'contact' ? 'true' : 'false';
-
-    // bolean untuk mengecek apakah route saat ini sesuai dengan parameter (cara canggih)
-    return request()->is('contact') ? 'true' : 'false';
-});
-
+Route::view('contact', 'contact');
 Route::view('about', 'about');
 Route::view('login', 'login');
