4e244ca9ab210910a32e3e40597224443f136a68
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-22 16:32:02 +0700

    Rute menampilkan halaman contact.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 route::get('/', function () {
-    return '<h1>Welcome</h1>';
+    return '<h1>Welcome</h1>'; // Ya kali ngedit nya di dalam return sini
 });
 
-route::get('/contact', function () {
-    return 'Contact Us';
+route::get('contact', function () {
+    return view('contact'); // setiap akses /contact akan menampilkan file contact.blade.php
 });
