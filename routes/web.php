ce5442360de66426f5e7b705df7eaff80af19999
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-22 16:41:06 +0700

    menambahkan rute baru untuk page yang ada dalam folder di dalam views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::view('contact', 'contact');
+
+Route::view('series/create', 'series.create');
+//  pada laravel untuk file yang dibuka bisa 
+//  menggunakan format :
+//  -   series/create; atau
+//  -   series.create
