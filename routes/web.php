19fa11a24a61302abf2c72817d4c1f5769d20433
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-22 23:40:50 +0700

    dasar active state pada navbar

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::view('contact', 'contact');
+Route::get('contact', function (Request $request) {
+    // return $request->path(); digunakan untuk menampilkan path dari route
+    // return request()->path(); digunakan untuk menampilkan path dari route
+
+    // boolean untuk mengecek apakah path dari route sesuai dengan parameter
+    // return request()->path() == 'contact' ? 'true' : 'false';
+
+    // bolean untuk mengecek apakah route saat ini sesuai dengan parameter (cara canggih)
+    return request()->is('contact') ? 'true' : 'false';
+});
+
 Route::view('about', 'about');
 Route::view('login', 'login');
