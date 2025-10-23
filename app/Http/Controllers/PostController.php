9662c192e6bd74f720fa4c9e75e916fd950dbf43
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-23 11:33:24 +0700

    mengganti metode penampilan database menggunakan model eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
         // Ambil data dari database
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = \App\post::where('slug', $slug)->firstOrFail(); 
+        // firstOrFail() akan mengembalikan 404 otomatis jika data tidak ditemukan
+
+        // Untuk validasi apakah post tersedia bisa menggunakan :
+        // 1. seperti ini
+        // if(!$post) {
+        //     abort(404);
+        // }
+
+        // 2. seperti ini
+        // if(is_null($post)) {
+        //     abort(404);
+        // }
+
+        
 
         // Logika untuk menampilkan postingan berdasarkan slug
         return view('posts.show', compact('post'));
