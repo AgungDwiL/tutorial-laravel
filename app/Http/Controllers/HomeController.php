826d3dd814c0a017ec91bec751f78d3f0484688b
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-23 07:09:28 +0700

    membuat mengganti syntax menggunakan compact() pada HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home', ['name' => request('name')]);
+        $name = "Agung";
+        // sekarang menggunakan compact
+        // compact('name') sama dengan ['name' => $name]
+        return view('home', compact('name'));
     }
 }
