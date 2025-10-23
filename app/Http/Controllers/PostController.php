be828dddcf7c0a1c49e4c747022c56c158c02ee9
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-23 11:08:57 +0700

    Koneksi ke database pada halaman post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,11 @@ class PostController extends Controller
 {
     public function show($slug)
     {
+        // Ambil data dari database
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+        
+        dd($post); // Debugging: menampilkan data postingan berdasarkan slug
+
         // Logika untuk menampilkan postingan berdasarkan slug
         return view('posts.show', compact('slug'));
     }
