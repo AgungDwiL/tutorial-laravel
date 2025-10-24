89c0fae0cad98a355a920f21f1f5538ed02ee431
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-24 15:12:50 +0700

    memperbaiki bug minor pada title halaman welcome user

--- resources/views/users/welcome.blade.php
+++ resources/views/users/welcome.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.master')
-@section('title-page', 'Welcome {{$user->name}}')
+@section('title-page')
+    Welcome {{$user->name}}
+@endsection
 
 @section('content')
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12 text-center">
-                <h1>Welcome, {{ $user->name }}!</h1>
+                <h1>Welcome, {{ $user->name }}</h1>
                 <p class="lead">We're glad to have you here.</p>
             </div>
         </div>
