58787d4c6690640e6db66297a3b4e6c916fea1e8
Agung Dwi Laksana <agung.laksana16@gmail.com>
2025-10-30 03:44:08 +0700

    add category_id to posts table

---
+++ database/migrations/2025_10_29_201912_add_category_id_to_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            
+            // Itu cara manual, cara gampangnya
+            // $table->foreignId('nama data pada tabel foreign_id')
+            // nanti langsung connect tanpa ada settingan di syntax bawah
+            // ex $table->foreignId('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
